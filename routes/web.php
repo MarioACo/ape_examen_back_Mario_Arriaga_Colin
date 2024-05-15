70969f5699549ea11272f62b887adf88730736e5
MarioACo <halo1232100@gmail.com>
2024-05-15 17:11:52 -0600

    feat(fix): fix cors conection

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\Cors;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [UserController::class, 'getAllUsers']);
+Route::get('/users', [UserController::class, 'getAllUsers'])->middleware(Cors::class);
+
